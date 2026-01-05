ae71a007cd6853f0eae3ee5fa2fe2abb6027d6b8
saliante01 <s.aliante01@ufromail.cl>
2026-01-04 21:34:15 -0300

    Init backend

---
+++ backend-chacrasdelsur/src/main/java/backend/chacrasdelsur/BackendChacrasdelsurApplication.java
@@ -0,0 +1,13 @@
+package backend.chacrasdelsur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChacrasdelsurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendChacrasdelsurApplication.class, args);
+    }
+
+}
---
+++ backend-chacrasdelsur/src/test/java/backend/chacrasdelsur/BackendChacrasdelsurApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.chacrasdelsur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChacrasdelsurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
